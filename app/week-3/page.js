1762366dae27959d7975dab4b3870006b2cf04ae
SurendraGhimire <151568945+SurendraGhimire@users.noreply.github.com>
2024-10-09 18:49:08 -0600

    Add files via upload

---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ItemList from "./item-list";
+const Page = () => {
+    return (
+      <main className="h-screen">
+        <h1 className="text-5xl font-bold mb-4">Shopping List</h1>
+        <ItemList />
+      </main>
+    );
+  };
+  export default Page;
