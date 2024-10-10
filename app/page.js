639a7943851103270fa8fa8aec77f3ada413805b
SurendraGhimire <151568945+SurendraGhimire@users.noreply.github.com>
2024-10-09 18:48:30 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,25 @@
-import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <div>
+import Link from 'next/link'
+export default function page() {
+    return(
+      <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">
-         Week 2
-      </Link>
-    </div>
+<li>
+  <Link href="./week-2" className="font-bold text-brown-600 hover:text-blue-400 shadow-lg">
+    Week 2
+  </Link>
+</li>
+
+<li>
+  <Link href="./week-3" className="font-bold text-brown-600 hover:text-blue-400 shadow-lg">
+    Week 3
+  </Link>
+</li>
+
+<li>
+  <Link href="./week-4" className="font-bold text-brown-600 hover:text-blue-400 shadow-lg">
+    Week 4
+  </Link>
+</li>
+      </main>
   );
 }
